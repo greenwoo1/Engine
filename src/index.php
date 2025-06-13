c260b3275687bb218d26bea1bc05282dda6d04c4
Anton Mazur <toxas99@icloud.com>
2025-06-13 14:38:13 +0300

    Finally xdebug works in Linux

--- src/index.php
+++ src/index.php
@@ -1,11 +1,22 @@
 <?php
 
-define('APP_DIR', __DIR__, '/');
+define('APP_DIR', __DIR__);
 
 require_once APP_DIR . '/app/Enums/HttpMethod.php';
 require_once APP_DIR . '/app/Router/Router.php';
 require_once APP_DIR . '/app/Router/routes.php';
 
+
+function calculateSquares(int $limit): array {
+    $result = [];
+    for ($i = 1; $i <= $limit; $i++) {
+        $result[] = $i * $i; // ← постав тут брейкпоінт
+    }
+    return $result;
+}
+
+$squares = calculateSquares(5);
+print_r($squares);
 
 
 
